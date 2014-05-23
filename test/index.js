7716d228e6a8954b766a4bc995c8dd488cf7b17e
Bodo Kaiser <i@bodokaiser.io>
2014-05-23 12:20:48 +0200

    added not found tests

--- test/index.js
+++ test/index.js
@@ -8,6 +8,21 @@ describe('lessie(path)', function() {
 
   before(helper.setup);
 
+  it('should respond not found', function(done) {
+    this.agent.get('/')
+      .expect(404, done);
+  });
+
+  it('should respond not found', function(done) {
+    this.agent.get('/.css')
+      .expect(404, done);
+  });
+
+  it('should respond not found', function(done) {
+    this.agent.get('/foobar.css')
+      .expect(404, done);
+  });
+
   it('should respond compiled stylesheet', function(done) {
     var self = this;
 
@@ -42,6 +57,47 @@ describe('lessie(options)', function() {
 
   describe('lessie({ path: "/styles" })', function() {
 
+    before(function() {
+      this.options = {
+        path: __dirname + '/styles'
+      };
+    });
+
+    before(helper.setup);
+
+    it('should respond not found', function(done) {
+      this.agent.get('/foobar.css')
+        .expect(404, done);
+    });
+
+    it('should respond compiled stylesheet', function(done) {
+      var self = this;
+
+      helper.compile('simple.less', function(err, stylesheet) {
+        if (err) throw err;
+
+        self.agent.get('/simple.css')
+          .expect('Content-Type', /css/)
+          .expect('Content-Length', stylesheet.length)
+          .expect(200, stylesheet, done);
+      });
+    });
+
+    it('should respond compiled stylesheet with its imports', function(done) {
+      var self = this;
+
+      helper.compile('imports.less', function(err, stylesheet) {
+        if (err) throw err;
+
+        self.agent.get('/imports.css')
+          .expect('Content-Type', /css/)
+          .expect('Content-Length', stylesheet.length)
+          .expect(200, stylesheet, done);
+      });
+    });
+
+    after(helper.destroy);
+
   });
 
   describe('lessie({ path: "/styles", prefix: "/stylesheets" })', function() {
@@ -54,6 +110,16 @@ describe('lessie(options)', function() {
     });
 
     before(helper.setup);
+
+    it('should respond not found', function(done) {
+      this.agent.get('/foobar.css')
+        .expect(404, done);
+    });
+
+    it('should respond not found', function(done) {
+      this.agent.get('/simple.css')
+        .expect(404, done);
+    });
 
     it('should respond compiled stylesheet in directory', function(done) {
       var self = this;
